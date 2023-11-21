124f9e82df806e107ff2d37408d7a9e5df091daa
HG20k <giahoang21205@gmail.com>
2023-11-21 19:39:34 +0700

    list-menu - css - add:

---
+++ javascript/src.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-list');
+const links = document.querySelectorAll('.nav-list li');
+
+hamburger.addEventListener('click', ()=>{
+   //Links
+    navLinks.classList.toggle('open');
+    links.forEach(link => {
+        link.classList.toggle('fade');
+    });
+
+    //Animation
+    hamburger.classList.toggle('toggle');
+});
